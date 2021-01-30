781afdc1773d25407184dc0edad116edb5b28ec5
Rui Chen <70863939+rchen8023@users.noreply.github.com>
2021-01-29 20:00:46 -0700

    Done Formatting for different screen size.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
+    $('#about').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '80px;'
+    });
+    
     $(function() {
       $('a[href*=#]:not([href=#])').click(function() { /*select the element where a href start with # */
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -13,6 +23,48 @@ $(document).ready(function(){
           }
         }
       });
+    });
+    
+    $('.bar-js').waypoint(function(direction) {
+        $('.bar-js').addClass('animated fadeInLeft');
+    }, {
+        offset: '75%'
+    });
+    
+    $('.photo-js').waypoint(function(direction) {
+        $('.photo-js').addClass('animated flipInY');
+    }, {
+        offset: '70%'
+    });
+    
+    $('.duty-1-js').waypoint(function(direction) {
+        $('.duty-1-js').addClass('animated fadeInRight');
+    }, {
+        offset: '60%'
+    });
+    
+    $('.duty-2-js').waypoint(function(direction) {
+        $('.duty-2-js').addClass('animated fadeInRight');
+    }, {
+        offset: '60%'
+    });
+    
+    $('.contact-js').waypoint(function(direction) {
+        $('.contact-js').addClass('animated fadeInDown');
+    }, {
+        offset: '70%'
+    });
+    
+    /*Mobile Nav*/
+    $('#nav-icon').click(function() {
+        var nav = $('#main-nav-js');
+        var icon = $('#nav-icon ion-icon');
+        nav.slideToggle(200);
+        if (icon[0].name == 'menu-outline') {
+            icon.attr('name','close-outline'); /* change name to new icon name*/
+        } else {
+            icon.attr('name','menu-outline');
+        }
     });
     
 });
